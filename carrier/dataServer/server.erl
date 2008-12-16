3417c59734bae00cffad22055f46b7efebc5bd6b
LTsongbin <LTsongbin@853c3d00-c778-11dd-b6f5-21ad4398c266>
2008-12-16 14:49:04 +0000

    runnable server/client server:start(). client:test(). then, we can get receive data from send.dat to recv.dat

--- carrier/dataServer/server.erl
+++ carrier/dataServer/server.erl
@@ -2,10 +2,15 @@
 -export([start/0]).
 
 start() ->
-    {ok, Listen} = gen_tcp:listen(9999, [binary, {packet, 2}, {active, true}]),
+    {ok, Listen} = gen_tcp:listen(9999, [binary, 
+					 {packet, 2}, 
+					 {active, true},
+					 {reuseaddr, true}]),
+    register(?MODULE, self()),
     spawn(fun() -> par_connect(Listen) end).
 
 par_connect(Listen) ->
+    %% io:format("new listen process:<<~p>>~n", self()),
     {ok, Socket} = gen_tcp:accept(Listen),
     spawn(fun() -> par_connect(Listen) end),
     loop(Socket).
@@ -18,24 +23,32 @@ loop(Socket) ->
 
 	    case Req of
 		{read, ChunkID, Begin, Size} ->
-		    do_read(Socket, ChunkID, Begin, Size);
+		    read_response(Socket, ChunkID, Begin, Size),
+		    io:format("read response over!~n");
 		{write, ChunkID} ->
-		    do_write(Socket, ChunkID);
+		    write_response(Socket, ChunkID);
 		_Any ->
 		    io:format("Server Unkown req:~p~n", [Req])
-	    end
+	    end;
+	{tcp_close, Socket} ->
+	    io:format("Read Req finish~n");
+	{ _, _} ->
+	    io:format("something unknown to server~n")
     end.
 
-do_read(Socket, ChunkID, Begin, Size) ->
+read_response(Socket, _ChunkID, Begin, Size) ->
     {ok, ListenData} = gen_tcp:listen(0, [binary, {packet, 2}, {active, true}]),
     {ok, Port} = inet:port(ListenData),
     gen_tcp:send(Socket, term_to_binary({ok, Port})),
 
     {ok, SocketData} = gen_tcp:accept(ListenData),
     {ok, Hdl} = file:open("send.dat", [raw, read, binary]),
-    loop(SocketData, Begin, Size).
+    send_it(SocketData, Hdl, Begin, Size).
 
-loop_data(SocketData) ->
+send_it(SocketData, Hdl, Begin, Size) ->
     {ok, Binary} = file:pread(Hdl, Begin, Size),
+    gen_tcp:send(SocketData, Binary).
 
 
+write_response(_Socket, _ChunkID) ->
+    io:format("Not implemented yet server:write_response/2~n").
