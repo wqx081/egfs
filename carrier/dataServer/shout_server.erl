efddda995b609430c6c295df1e6106dc2f2f9853
LTsongbin <LTsongbin@853c3d00-c778-11dd-b6f5-21ad4398c266>
2008-12-22 01:30:39 +0000

    try to add write

--- carrier/dataServer/shout_server.erl
+++ carrier/dataServer/shout_server.erl
@@ -91,6 +91,45 @@ get_file_size(File) ->
     end.
 
 
-write_response(_Socket, _ChunkID) ->
-    ?DEBUG("[shoutServer]: response for write~n", []),
-    ?DEBUG("[shoutServer]: response for write has not been implemented yet.~n", []).
+write_response(Socket, _ChunkID) ->
+    {ok, ListenData} = gen_tcp:listen(0, [binary, {packet, 2}, {active, once}]),
+    Reply = inet:port(LinstenData),
+    gen_tcp:send(Socket, term_to_binary(Reply)),
+
+    {ok, SocketData} = gen_tcp:accept(ListenData),
+    {ok, Hdl} = file:open(?TO_WRITE, [binary, raw, write]),
+
+    process_flag(trap_exit, true),
+    Child = spawn_link(fun() -> receive_data(self(), SocketData, Hdl, 0) end),
+
+    loop_receive_control(Child, Socket).
+
+loop_receive_control(Child, Socket) ->
+    receive
+	{tcp, Socket, Binary} ->
+	    Term = binary_to_term(Binary),
+	    case Term of
+		{finish, Size} ->
+		    ?DEBUG("[shoutServer]: notify from client that the receive porcess finished, size = ~p~n", Size);
+		{stop, _Why} ->
+		    ?DEBUG("[shoutServer]: notify from client that the receive process should be stopped~n", []),
+		    Child ! {stop, _Why};
+		_ ->
+		    ?DEBUG("[shoutServer]: unkown notify from client :~p~n", Term)
+	    end;
+	{tcp_close, Socket} ->
+	    ?DEBUG("[shoutServer]: client has closed the controll socket")
+    end.
+
+receive_data(Parent, SocketData, Hdl, Len) ->
+    receive
+	{tcp, SocketData, Binary} ->
+	    Len2 = Len + size(Binary),
+	    write_file(Hdl, Binary),
+	    receive_data(SocketData, Hdl, Len2);
+	{tcp_close, _Why} ->
+	    file:delete(?TO_WRITE),
+	    ?DEBUG("[shoutServer]: client close socket.~n", []);
+	{stop, Parent} ->
+	    ?DEBUG("[shoutServer]: stop signal from parent~n", []);
+    end.
