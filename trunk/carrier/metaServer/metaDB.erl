b8dc0cee5d70f9e1aa173ac1d393a4459b507ad3
gnibrE <gnibrE@853c3d00-c778-11dd-b6f5-21ad4398c266>
2008-12-24 02:29:28 +0000

    can run.

--- trunk/carrier/metaServer/metaDB.erl
+++ trunk/carrier/metaServer/metaDB.erl
@@ -5,7 +5,6 @@
 -module(metaDB).
 -import(lists, [foreach/2]).
 -import(util,[for/3]).
-
 %%
 %% Include files
 %%
@@ -17,13 +16,6 @@
 %-export([]).
 -compile(export_all).
 
-%tables  record to create table.
--record(filemetaTable, {fileid, filename, filesize, chunklist, createT, modifyT, acl}).
--record(chunkmappingTable, {chunkid, chunklocations}).
--record(clientinfo, {clientid, modes}).   % maybe fileid?
--record(filesessionTable, {fileid, client}).
-
-
 %%
 %% API Functions
 %%
@@ -33,24 +25,34 @@ do_this_once() ->
     mnesia:start(),
     mnesia:create_table(filemeta, 	%table name 
                         [			
-                         {attributes, record_info(fields, filemetaTable)},%table content
+                         {attributes, record_info(fields, filemeta)},%table content
                          {disc_copies,[node()]}                         
                         ]
                        ),
-    mnesia:create_table(chunkmapping, [{attributes, record_info(fields, chunkmappingTable)},
+    mnesia:create_table(filemeta_s, 	%table name 
+                        [			
+                         {attributes, record_info(fields, filemeta_s)},%table content
+                         {disc_copies,[node()]}                         
+                        ]
+                       ),
+    mnesia:create_table(chunkmapping, [{attributes, record_info(fields, chunkmapping)},
                                        {disc_copies,[node()]}
                                       ]),
 %%     mnesia:create_table(clientinfo, [{attributes, record_info(fields, clientinfoTable)},
 %%                                      {disc_copies,[node()]}
 %%                                     ]),
-    mnesia:create_table(filesession, [{attributes, record_info(fields, filesessionTable)},
+%%     mnesia:create_table(filesession, [{attributes, record_info(fields, filesession)},
+%%                                       {disc_copies,[node()]}
+%%                                      ]),
+    mnesia:create_table(hostinfo, [{attributes, record_info(fields, hostinfo)},
                                       {disc_copies,[node()]}
                                      ]),
+    
     mnesia:stop().
 
 start_mnesia()->
     mnesia:start(),
-    mnesia:wait_for_tables([filemeta, chunkmapping,filesession], 30000).
+    mnesia:wait_for_tables([filemeta,filemeta_s,chunkmapping,hostinfo], 30000).
 
 
 %%
@@ -93,9 +95,6 @@ do(Q) ->
 %% 			     X#filemeta.fileid < 250
 %% 				]));
 
-%-record(filemetaTable, {fileid, filename, filesize, chunklist, createT, modifyT, acl}).
-%-record(chunkmappingTable, {chunkid, chunklocations}).
-
 example_tables() ->
     [%% The filemeta table
      %			fileid,  filename, 			filesize, chunklist, createT, modifyT, acl
@@ -103,17 +102,22 @@ example_tables() ->
      {filemeta, 1,   "egfs://e:/copy/test.txt",3,[1],"today,Dec,12","yestoday,Dec,11","acl"},
      
      %% The chunkmapping table
-     {chunkmapping,0,[localhost]},
-     {chunkmapping,1,[localhost]}
+%%      {chunkmapping,0,[localhost]},
+%%      {chunkmapping,1,[localhost]}
+     
+     
+     {hostinfo,data_server,abc,1000000,2000000}
+     
     ].
 
 example_table_filemeta(X)->
 %    {filemeta, X,   "e:/copy/test",3,[X],"today,Dec,12","yestoday,Dec,11","acl"}.
-#filemetaTable{fileid=X,filename=["e:/copy/test",X],filesize=3,chunklist=[X],createT="today,Dec,12",modifyT="yestoday,Dec,11",acl="acl"}.
+#filemeta{fileid=X,filename=["e:/copy/test",X],filesize=3,chunklist=[X],createT="today,Dec,12",modifyT="yestoday,Dec,11",acl="acl"}.
 
 clear_tables()->
     mnesia:clear_table(filemeta),
-    mnesia:clear_table(filesession),
+    mnesia:clear_table(filemeta_s),
+    mnesia:clear_table(hostinfo),
     mnesia:clear_table(chunkmapping).
 
 
@@ -127,28 +131,45 @@ clear_tables()->
 reset_tables() ->
     mnesia:clear_table(filemeta),
     mnesia:clear_table(chunkmapping),
+    mnesia:clear_table(filemeta_s),
+	mnesia:clear_table(clientinfo),
+
     F = fun() ->
 		foreach(fun mnesia:write/1, example_tables())
-	end,
+		end,
     mnesia:transaction(F).
 
-insert_ten_thousand()->
+insert_ten_thousand(X)->
     mnesia:clear_table(filemeta),
-    F=fun()->              
-              util:for(1,10,fun(I)->mnesia:write(example_table_filemeta(I))end)
-    end.      
+           
+    util:for(1,X,
+             fun(I)->insert_filemeta_sample(I) end
+			).
+          
 
+insert_filemeta_sample(X)->
+    Row = example_table_filemeta(X),
+    F = fun()->
+                mnesia:write(Row)
+        end,
+    mnesia:transaction(F).
 
-for_test() ->
-   
-    
-    util:for(1,10,fun(I)->I*I end ).
 
 %filemeta    {fileid	client}
 %add item
 add_filemeta_item(Fileid, FileName) ->
-    Row = #filemetaTable{fileid=Fileid, filename=FileName, filesize=0, chunklist=[], 
-                         createT=term_to_binary(erlang:localtime(), modifyT=term_to_binary(erlang:localtime(), "acl"},
+    Row = #filemeta{fileid=Fileid, filename=FileName, filesize=0, chunklist=[], 
+                         createT=term_to_binary(erlang:localtime()), modifyT=term_to_binary(erlang:localtime()),acl="acl"},
+    F = fun() ->
+		mnesia:write(Row)
+	end,
+    mnesia:transaction(F).
+
+%filemeta_s    {fileid	client}
+%add item
+add_filemeta_s_item(Fileid, FileName) ->
+    Row = #filemeta_s{fileid=Fileid, filename=FileName, filesize=0, chunklist=[], 
+                         createT=term_to_binary(erlang:localtime()), modifyT=term_to_binary(erlang:localtime()),acl="acl"},
     F = fun() ->
 		mnesia:write(Row)
 	end,
@@ -157,7 +178,7 @@ add_filemeta_item(Fileid, FileName) ->
 %filesession    {fileid	client}
 %add item
 add_filesession_item(Fileid, Client) ->
-    Row = #filesessionTable{fileid=Fileid, client=Client},
+    Row = #filesession{fileid=Fileid, client=Client},
     F = fun() ->
 		mnesia:write(Row)
 	end,
@@ -172,35 +193,57 @@ remove_filesession_item(Fileid) ->
     mnesia:transaction(F).
 
 
+%%------------------------------------------------------------------------------------------
+%% select function
+%% all kinds 
+%%------------------------------------------------------------------------------------------ 
+select_all_from_anyTable(T)->
+    do(qlc:q([
+              X||X<-mnesia:table(T)
+              ])).  %result [L]
+
 %look up.
 select_from_filesession(Fileid) ->    %result [L]
     do(qlc:q([
               X||X<-mnesia:table(filesession),X#filesession.fileid =:= Fileid
               ])).
 
-
-select_allfrom_filesession() ->
-    do(qlc:q([X || X <- mnesia:table(filesession)])).   %result [L]
-
-%look up.
-select_allfrom_chunkmapping() ->
-    do(qlc:q([X || X <- mnesia:table(chunkmapping)])).   %result [L]
-
-%look up.
-select_allfrom_filemeta() ->    
-    do(qlc:q([X || X <- mnesia:table(filemeta)])).   %result [L]
-
-select_from_filemeta(S) ->    %result [L]
+select_from_filemeta(FileID) ->    %result [L]
     do(qlc:q([
-              X||X<-mnesia:table(filemeta),X#filemeta.fileid =:= S
+              X||X<-mnesia:table(filemeta),X#filemeta.fileid =:= FileID
+              ])).
+
+select_all_from_filemeta_s(FileID)->
+    do(qlc:q([
+              X||X<-mnesia:table(filemeta_s),X#filemeta_s.fileid =:= FileID
               ])).
 
 %FileName - > fileid
+% @spec select_fileid_from_filemeta(FileName) ->  fileid
+% fileid -> binary().
+
 select_fileid_from_filemeta(FileName) ->
     do(qlc:q([X#filemeta.fileid || X <- mnesia:table(filemeta),
-                                   X#filemeta.filename =:= FileName
-                                   
+                                   X#filemeta.filename =:= FileName                                   
                                    ])).   %result [L]
+
+
+select_fileid_from_filemeta_s(FileName) ->
+    do(qlc:q([X#filemeta_s.fileid || X <- mnesia:table(filemeta_s),
+                                   X#filemeta_s.filename =:= FileName                                   
+                                   ])).   %result [L]
+
+
+select_test(FileID) ->
+    do(qlc:q([X#filemeta.fileid || X <- mnesia:table(filemeta),
+                                   X#filemeta.fileid =:= FileID                                   
+                                   ])).   %result [L]
+
+select_nodeip_from_chunkmapping(ChunkID) ->
+    do(qlc:q([X#chunkmapping.chunklocations || X <- mnesia:table(chunkmapping),
+                                   X#chunkmapping.chunkid =:= ChunkID
+             ])).   %result [L]
+
 
 %clear chunks from filemeta
 reset_file_from_filemeta(Fileid) ->
@@ -215,6 +258,18 @@ reset_file_from_filemeta(Fileid) ->
 	end,
     mnesia:transaction(F).
 
+% look_up_filesession(FileID, ClientID) -> w | r | a
+% FileID = binary
+% ClientID = binary
+look_up_filesession(FileID, ClientID) ->
+	case select_from_filesession(FileID) of
+		% file hasn't been opened yet, so open operation is permitted.
+		[clientinfo, [ClientInfo]] when (ClientInfo#clientinfo.clientid =:= ClientID) -> 
+				ClientInfo#clientinfo.modes,
+                {ok, <<FileID:64>>};   
+		% file has been opened by other processes, so open operation is denied.
+		[_] -> {error, "opened already"}
+	end.
 
 %% demo(reorder) ->
 %%     do(qlc:q([X#filemeta.filename || X <- mnesia:table(filemeta),
--- trunk/carrier/metaServer/metaformat.hrl
+++ trunk/carrier/metaServer/metaformat.hrl
@@ -1,2 +1,12 @@
+
+
+%tables  record to create table.
+
 -record(filemeta, {fileid, filename, filesize, chunklist, createT, modifyT, acl}).
+-record(filemeta_s, {fileid, filename, filesize, chunklist, createT, modifyT, acl}).
 -record(chunkmapping, {chunkid, chunklocations}).
+
+-record(clientinfo, {clientid, modes}).   % maybe fileid?
+-record(filesession, {fileid, client}).
+-record(hostinfo,{ip,host,freespace,totalspace}).
+
