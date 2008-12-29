83bb048af74033b4f5bd5fc311b80153a8a6ac70
gnibrE <gnibrE@853c3d00-c778-11dd-b6f5-21ad4398c266>
2008-12-29 12:55:50 +0000

    增加了data server的启动汇报接口 增加了文件删除接口

--- carrier/metaServer/metagenserver.erl
+++ carrier/metaServer/metagenserver.erl
@@ -3,7 +3,13 @@
 
 -include("../include/egfs.hrl").
 
--import(metaserver, [do_open/3, do_allocate_chunk/2, do_register_chunk/4, do_get_chunk/2, do_close/2]).
+-import(metaserver, [do_open/3, 
+                     do_allocate_chunk/2, 
+                     do_register_chunk/4, 
+                     do_get_chunk/2, 
+                     do_close/2,
+                     do_dataserver_bootreport/2,
+                     do_delete/1]).
 
 -export([start/0,stop/0,terminate/2]).
 -export([init/1, handle_call/3, handle_cast/2,handle_info/2]).
@@ -25,7 +31,7 @@ start() ->
 stop() ->
     gen_server:cast(?GM, stop).
 
-terminate(Reason, State) ->
+terminate(_Reason, _State) ->
     io:format("meta server terminating~n").
 
 
@@ -48,13 +54,14 @@ handle_call({allocatechunk, FileID}, {From, _}, State) ->
 %% Returns: 
 %% --------------------------------------------------------------------
 handle_call({registerchunk, FileID, ChunkID, ChunkUsedSize, NodeList},
-            From, State) ->    
+            _From, State) ->    
+    io:format("~p",[time()]),
     io:format("inside handle_call_registerchunk,FileID:~p,ChunkID:~p,ChunkUsedSize:~p~n",[FileID,ChunkID,ChunkUsedSize]),
     io:format("NodeList~p~n",[list_to_tuple(NodeList)]),
     Reply = do_register_chunk(FileID, ChunkID, ChunkUsedSize, NodeList),
     {reply, Reply, State};
 
-handle_call({locatechunk, FileID, ChunkIndex}, From, State) ->
+handle_call({locatechunk, FileID, ChunkIndex}, _From, State) ->
     io:format("inside handle_call_locatechunk,FileID:~p,ChunkIndex:~p~n",[FileID,ChunkIndex]),
     Reply = do_get_chunk(FileID, ChunkIndex),
     {reply, Reply, State};
@@ -64,29 +71,46 @@ handle_call({close, FileID}, {From, _}, State)->
 	Reply = do_close(FileID, From),
     {reply, Reply, State};
 
-handle_call(_, {From, _}, State)->
+handle_call({delete,FileId},{_From,_},State) ->
+    io:format("inside handle_call_delete,FileID:~p~n",[FileId]),
+	Reply = do_delete(FileId),
+    {reply, Reply, State};
+
+handle_call({bootreport,HostInfoRec, ChunkList},{_From,_},State) ->
+    io:format("inside handle_call_bootreport,FileID:~p~n",[HostInfoRec]),
+	Reply = do_dataserver_bootreport(HostInfoRec, ChunkList),
+    {reply, Reply, State};
+
+
+handle_call(_, {_From, _}, State)->
     io:format("inside handle_call_error~n"),
 	Reply = {error, "undefined handler"},
     {reply, Reply, State}.
+
+
+
 
 handle_cast(stop, State) ->
     io:format("meta server stopping~n"),
     {stop, normal, State}.
 
-handle_info(Info, State) ->
+handle_info(_Info, State) ->
     {noreply, State}.
 
-%"client api" methods
+%%%"client api" methods
 % write operation sequence:
 % 1. open(client) =>
 % 2. writeAllocate(client) =>
-% 3. registerChunk(chunkserver) =>
+% ****3. registerChunk(chunkserver) =>
 % 4. close(client)
 
 % read operation sequence:
 % 1. open(client) =>
 % 2. locateChunk(client) =>
 % 3. close(client)
+
+%% delete operation:
+%% 1. delete(FileID)
 
 open(FileName,Mode) ->
     gen_server:call(?GM, {open, FileName,Mode}).
@@ -100,9 +124,24 @@ writeAllocate(FileID) ->
 locateChunk(FileID, ChunkIndex)->
     gen_server:call(?GM, {locatechunk, FileID, ChunkIndex}).
 
+close(FileID)->
+    gen_server:call(?GM, {close, FileID}).
+
+delete(FileID)->
+    gen_server:call(?GM, {delete, FileID}).
+
+%% "chunk server api" methods
+%% write operation
+%% 3. registerChunk(chunkserver) =>
+
+%% boot report methods
+%% 1. bootReport(HostInfoRec, ChunkList)
+
 registerChunk(FileID, ChunkID, ChunkUsedSize, NodeList)->
     gen_server:call(?GM, {registerchunk, FileID, ChunkID, ChunkUsedSize, NodeList}).
 
-close(FileID)->
-    gen_server:call(?GM, {close, FileID}).
+%%
+%% bootReport(HostInfoRec, ChunkList) => {ok, OrphanChunkList} | {error, "Reason for error"}
+bootReport(HostInfoRec, ChunkList)->
+    gen_server:call(?GM, {bootreport, HostInfoRec, ChunkList}).
 
