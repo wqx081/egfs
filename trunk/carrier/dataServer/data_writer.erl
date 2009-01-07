9d7b48598e3b3701a253950e80d7c164f30fd8ee
LTsongbin <LTsongbin@853c3d00-c778-11dd-b6f5-21ad4398c266>
2009-01-07 14:28:04 +0000

    full

--- trunk/carrier/dataServer/data_writer.erl
+++ trunk/carrier/dataServer/data_writer.erl
@@ -38,9 +38,37 @@ init_write_process(Listen) ->
 write_process_tail(FileID, ChunkIndex, ChunkID, Listen) ->
     {ok, Socket, ListenData, Parent} = init_write_process(Listen),
     Child = spawn_link(fun() -> receive_it_tail(Parent, ListenData, ChunkID) end),
-    loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, 0).
+    Result = loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, 0),
+    write_aftercare(Result, ChunkID).
 
 loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, State) ->
+    receive
+	{finish, Child, Len} ->
+	    ?DEBUG("[~p, ~p]: write transfer finish, ~pBytes~n", [?MODULE, ?LINE, Len]),
+	    {ok, Name} = get_file_name(ChunkID),
+	    chunk_db:insert_chunk_info(ChunkID, FileID, Name, Len),
+	    gen_tcp:send(Socket, term_to_binary({check, Len})),
+	    wait_for_check_result(Socket);
+	{error, Child, Why} ->
+	    ?DEBUG("[~p, ~p]: data transfer error~p~n", [?MODULE, ?LINE, Why]),
+	    {error, data_receive, Why};
+	{tcp, Socket, Binary} ->
+	    Term = binary_to_term(Binary),
+	    case Term of 
+		{stop, Why} ->
+		    ?DEBUG("[data_server, ~p]: write stop msg from client.~n", [?LINE]),
+		    Child ! {stop, self(), Why},
+		    gen_tcp:close(Socket),
+		    {ok, stop, "write stop from writer"};
+		_Any ->
+		    loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, State)
+	    end;
+	{tcp_closed, Socket} ->
+	    ?DEBUG("[~p, ~p]: write control broken~n", [?MODULE, ?LINE]),
+	    {error, write_control, "write control broken"};
+	Any ->
+	    ?DEBUG("[~p, ~p]: unkown msg ~p~n", [?MODULE, ?LINE, Any]),
+	    loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, State)
     ok.
 
 receive_it_tail(Parent, ListenData, ChkID) ->
@@ -59,6 +87,9 @@ loop_receive(Parent, SocketData, Hdl, Len) ->
 	{tcp_closed, SocketData} ->
 	    file:close(Hdl),
 	    Parent ! {finish, self(), Len};
+	{stop, Parent, _Why} ->
+	    file:close(Hdl),
+	    get_tcp:close(SocketData);
 	Any ->
 	    ?DEBUG("[data_server, ~p]:loop Any:~p~n", [?LINE, Any])
     end. 
@@ -81,8 +112,18 @@ write_process(FileID, ChunkIndex, ChunkID, Listen, Next) ->
     {ok, Socket, ListenData, Parent} = init_write_process(Listen),
     {ok, NextSocket, Next_DataPort} = connect_to_next(Next),
     Child = spawn_link(fun() -> receive_it(Parent, ListenData, Next_DataPort, ChunkID) end),
-    loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, 0),
+    Result = loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, 0),
+    write_aftercare(Result, ChunkID),
     gen_tcp:close(NextSocket).
+
+write_aftercare(Result, ChunkID) ->
+    case Result of
+	{ok, check} ->
+	    ok;
+	_Any ->
+	    rm_pending_chunk(ChunkID),
+	    chunk_db:remove_chunk_info(ChunkID)
+    end.
 
 loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State) ->
     receive
@@ -98,7 +139,8 @@ loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State
 	    gen_tcp:send(Socket, term_to_binary({error, "data transfer error"})),
 	    gen_tcp:send(NextSocket, term_to_binary({stop, "data transfer error"})),
 	    gen_tcp:close(NextSocket),
-	    Child ! {die, self()};
+	    Child ! {die, self()},
+	    {error, data_receive, Why};
 	{tcp, Socket, Binary} ->
 	    gen_tcp:send(NextSocket, Binary),
 	    loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State);
@@ -108,31 +150,54 @@ loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State
 	{tcp_closed, Socket} ->
 	    rm_pending_chunk(ChunkID),
 	    gen_tcp:send(NextSocket, term_to_binary({stop, "data control error"})),
-	    gen_tcp:close(NextSocket);
+	    gen_tcp:close(NextSocket),
+	    {error, control_socket, "control socket broken"};
 	{tcp_closed, NextSocket} ->
-	    rm_pending_chunk(ChunkID),
-	    gen_tcp:send(Socket, term_to_binary({error, "next data control broken"}));
+	    %%rm_pending_chunk(ChunkID),
+	    gen_tcp:send(Socket, term_to_binary({error, "next data control broken"})),
+	    {error, next_control_socket, "next control socket broken"};
 	Any ->
 	    ?DEBUG("[~p, ~p] unkown control msg:~p ~n", [?MODULE, ?LINE, Any]),
 	    loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State)
     end.
 
-wait_for_check(Socket, NextSocket, _ChunkID, Len, State) ->
+wait_for_check(Socket, NextSocket, ChunkID, Len, State) ->
     receive
 	{tcp, NextSocket, Binary} ->
 	    Term = binary_to_term(Binary),
 	    Case Term of
 		{check, Len} ->
+		    Result = {ok, check},
+		    gen_tcp:send(NextSocket, term_to_binary(Result)),
 		    gen_tcp:send(Socket, Binary),
-		    {ok, "check_ok"};
+		    wait_for_check_result(Socket);
 		_Other ->
-		    gen_tcp:send(Socket, term_to_binary({error, "check error"})),
-		    {error, "check_fail"}
+		    Result = {error, check, "length error"},
+		    gen_tcp:send(Socket, term_to_binary(Result)),
+		    gen_tcp:send(NextSocket, term_to_binary(Result)),
+		    Result
 	    end;
 	Any ->
 	    wait_for_check(Socket, NextSocket, ChunkID, Len, State)
     end.
 
+wait_for_check_result(Socket) ->
+    receive
+	{tcp, Socket, Binary} ->
+	    Term = binary_to_term(Binary),
+	    Case Term of
+		{ok, check} ->
+		    Term;
+	        {error, check, Why} ->
+		    Term;
+		Other ->
+		    {error, check, Other}
+	    end;
+	{tcp_closed, Socket} ->
+	    {error, check, "broken socket"};
+	Any ->
+	    wait_for_check_result(Socket)
+    end.
 
 receive_it(Parent, ListenData, Next_IP, Next_DataPort, ChunkID) ->
     {ok, SocketData} = gen_tcp:accept(ListenData),
