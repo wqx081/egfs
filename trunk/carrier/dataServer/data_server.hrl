4823b546297a77fe46e6f084701f02f3bd5ea004
LTsongbin <LTsongbin@853c3d00-c778-11dd-b6f5-21ad4398c266>
2009-01-08 01:18:37 +0000

    ok

--- trunk/carrier/dataServer/data_server.hrl
+++ trunk/carrier/dataServer/data_server.hrl
@@ -4,5 +4,6 @@
 -record(garbageinfo, {chunk_id, insert_time}).
 
 -define(SERVER_NAME, data_server1).
+-define(STRIP_SIZE, 8192).
 -define(TOTAL_SPACE, 21474836480).
 -define(GARBAGE_AUTO_COLLECT_PERIOD,	7000).    % 5000 milisecond = 5 second
--- trunk/carrier/dataServer/data_writer.erl
+++ trunk/carrier/dataServer/data_writer.erl
@@ -1,12 +1,13 @@
 -module(data_writer).
 -include("../include/egfs.hrl").
+-include("data_server.hrl").
+-import(chunk_db).
+-import(toolkit).
 -export([handle_write/4]).
-
--define(STRIP_SIZE, 8192).
 
 generate_write_reply()->
     {ok, Listen} = gen_tcp:listen(0, [binary, {packet, 2}, {active, true}]),
-    {ok, IP_Addr} = get_local_addr(),
+    {ok, IP_Addr} = toolkit:get_local_addr(),
     {ok, Port} = inet:port(Listen),
     {ok, Listen, {ok, IP_Addr, Port}}.
     
@@ -18,7 +19,7 @@ handle_write(FileID, ChunkIndex, ChunkID, [H|T]) ->
     {ok, Listen, Reply} = generate_write_reply(),
     Next = gen_server:call(H, {writechunk, FileID, ChunkIndex, chunkID, T}),
     {ok, _Next_IP, _Next_Port} = Next,
-    spawn(fun() -> write_process(FileID, ChunkIndex, ChunkID, Listen, Next) end),
+    spawn(fun() -> relay_write_process(FileID, ChunkIndex, ChunkID, Listen, Next) end),
     Reply.
 
 
@@ -35,7 +36,7 @@ init_write_process(Listen) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%            Write to Itself
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-write_process_tail(FileID, ChunkIndex, ChunkID, Listen) ->
+write_process(FileID, ChunkIndex, ChunkID, Listen) ->
     {ok, Socket, ListenData, Parent} = init_write_process(Listen),
     Child = spawn_link(fun() -> receive_it_tail(Parent, ListenData, ChunkID) end),
     Result = loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, 0),
@@ -45,7 +46,7 @@ loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, State) ->
     receive
 	{finish, Child, Len} ->
 	    ?DEBUG("[~p, ~p]: write transfer finish, ~pBytes~n", [?MODULE, ?LINE, Len]),
-	    {ok, Name} = get_file_name(ChunkID),
+	    {ok, Name} = toolkit:get_file_name(ChunkID),
 	    chunk_db:insert_chunk_info(ChunkID, FileID, Name, Len),
 	    gen_tcp:send(Socket, term_to_binary({check, Len})),
 	    wait_for_check_result(Socket);
@@ -69,12 +70,12 @@ loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, State) ->
 	Any ->
 	    ?DEBUG("[~p, ~p]: unkown msg ~p~n", [?MODULE, ?LINE, Any]),
 	    loop_write_control_tail(Socket, Child, FileID, ChunkIndex, ChunkID, State)
-    ok.
+    end.
 
 receive_it_tail(Parent, ListenData, ChkID) ->
     {ok, SocketData} = gen_tcp:accept(ListenData),
     gen_tcp:close(ListenData),
-    {ok, Hdl} = get_file_handle(write, ChkID),
+    {ok, Hdl} = toolkit:get_file_handle(write, ChkID),
     loop_receive(Parent, SocketData, Hdl, 0),
     gen_tcp:close(SocketData).
 
@@ -102,17 +103,16 @@ connect_to_next({ok, Next_IP, Next_Port}) ->
     receive 
 	{tcp, NextSocket, Binary} ->
 	    {ok, Next_DataPort} = binary_to_term(Binary),
-	    Reply = {ok, NextSocket, Next_DataPort};
-	Any ->
-	    Reply = {error, "cann't get next data port", []}
-    end;
-    Reply.
+	    {ok, NextSocket, Next_IP, Next_DataPort};
+	_Any ->
+	    {error, next_connect, "cann't get next data port", []}
+    end.
 	    
-write_process(FileID, ChunkIndex, ChunkID, Listen, Next) ->
+relay_write_process(FileID, ChunkIndex, ChunkID, Listen, Next) ->
     {ok, Socket, ListenData, Parent} = init_write_process(Listen),
-    {ok, NextSocket, Next_DataPort} = connect_to_next(Next),
-    Child = spawn_link(fun() -> receive_it(Parent, ListenData, Next_DataPort, ChunkID) end),
-    Result = loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, 0),
+    {ok, NextSocket, Next_IP, Next_DataPort} = connect_to_next(Next),
+    Child = spawn_link(fun() -> relay_receive_it(Parent, ListenData, Next_IP, Next_DataPort, ChunkID) end),
+    Result = loop_relay_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, 0),
     write_aftercare(Result, ChunkID),
     gen_tcp:close(NextSocket).
 
@@ -121,21 +121,21 @@ write_aftercare(Result, ChunkID) ->
 	{ok, check} ->
 	    ok;
 	_Any ->
-	    rm_pending_chunk(ChunkID),
+	    toolkit:rm_pending_chunk(ChunkID),
 	    chunk_db:remove_chunk_info(ChunkID)
     end.
 
-loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State) ->
+loop_relay_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State) ->
     receive
 	{finish, Child, Len} ->
-	    {ok, Name} = get_file_name(ChunkID),
+	    {ok, Name} = toolkit:get_file_name(ChunkID),
 	    chunk_db:insert_chunk_info(ChunkID, FileID, Name, Len),
 	    %% Result = report_metaServer(FileID, ChunkIndex, ChunkID, Len),
 	    Child ! {die, self()},
 	    wait_for_check(Socket, NextSocket, ChunkID, Len, 0);
 	{error, Child, Why} ->
 	    ?DEBUG("[~p, ~p] error from child(data transfer) ~n", [?MODULE, ?LINE]),
-	    rm_pending_chunk(ChunkID),
+	    toolkit:rm_pending_chunk(ChunkID),
 	    gen_tcp:send(Socket, term_to_binary({error, "data transfer error"})),
 	    gen_tcp:send(NextSocket, term_to_binary({stop, "data transfer error"})),
 	    gen_tcp:close(NextSocket),
@@ -143,12 +143,12 @@ loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State
 	    {error, data_receive, Why};
 	{tcp, Socket, Binary} ->
 	    gen_tcp:send(NextSocket, Binary),
-	    loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State);
+	    loop_relay_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State);
 	{tcp, NextSocket, Binary} ->
 	    gen_tcp:send(Socket, Binary),
-	    loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State);
+	    loop_relay_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State);
 	{tcp_closed, Socket} ->
-	    rm_pending_chunk(ChunkID),
+	    toolkit:rm_pending_chunk(ChunkID),
 	    gen_tcp:send(NextSocket, term_to_binary({stop, "data control error"})),
 	    gen_tcp:close(NextSocket),
 	    {error, control_socket, "control socket broken"};
@@ -158,14 +158,14 @@ loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State
 	    {error, next_control_socket, "next control socket broken"};
 	Any ->
 	    ?DEBUG("[~p, ~p] unkown control msg:~p ~n", [?MODULE, ?LINE, Any]),
-	    loop_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State)
+	    loop_relay_write_control(Socket, NextSocket, Child, FileID, ChunkIndex, ChunkID, State)
     end.
 
 wait_for_check(Socket, NextSocket, ChunkID, Len, State) ->
     receive
 	{tcp, NextSocket, Binary} ->
 	    Term = binary_to_term(Binary),
-	    Case Term of
+	    case Term of
 		{check, Len} ->
 		    Result = {ok, check},
 		    gen_tcp:send(NextSocket, term_to_binary(Result)),
@@ -177,7 +177,7 @@ wait_for_check(Socket, NextSocket, ChunkID, Len, State) ->
 		    gen_tcp:send(NextSocket, term_to_binary(Result)),
 		    Result
 	    end;
-	Any ->
+	_Any ->
 	    wait_for_check(Socket, NextSocket, ChunkID, Len, State)
     end.
 
@@ -185,47 +185,47 @@ wait_for_check_result(Socket) ->
     receive
 	{tcp, Socket, Binary} ->
 	    Term = binary_to_term(Binary),
-	    Case Term of
+	    case Term of
 		{ok, check} ->
 		    Term;
-	        {error, check, Why} ->
+	        {error, check, _Why} ->
 		    Term;
 		Other ->
 		    {error, check, Other}
 	    end;
 	{tcp_closed, Socket} ->
 	    {error, check, "broken socket"};
-	Any ->
+	_Any ->
 	    wait_for_check_result(Socket)
     end.
 
-receive_it(Parent, ListenData, Next_IP, Next_DataPort, ChunkID) ->
+relay_receive_it(Parent, ListenData, Next_IP, Next_DataPort, ChunkID) ->
     {ok, SocketData} = gen_tcp:accept(ListenData),
     gen_tcp:close(ListenData),
-    {ok, Hdl} = get_file_handle(write, ChunkID),
+    {ok, Hdl} = toolkit:get_file_handle(write, ChunkID),
     {ok, NextSocketData} = gen_tcp:connect(Next_IP, Next_DataPort, [binary, {packet, 2}, {active, true}]),
-    loop_receive(Parent, SocketData, NextSocketData, Hdl, 0),
+    loop_relay_receive(Parent, SocketData, NextSocketData, Hdl, 0),
     gen_tcp:close(NextSocketData).
 
-loop_receive(Parent, SocketData, NextSocketData, Hdl, Len) ->
+loop_relay_receive(Parent, SocketData, NextSocketData, Hdl, Len) ->
     receive
 	{tcp, SocketData, Binary} ->
 	    Len2 = Len + size(Binary),
 	    file:write(Hdl, Binary),
 	    gen_tcp:send(NextSocketData, Binary),
-	    loop_receive(Parent, SocketData, NextSocketData, Hdl, Len2);
+	    loop_relay_receive(Parent, SocketData, NextSocketData, Hdl, Len2);
 	{tcp_closed, SocketData} ->
 	    file:close(Hdl),
 	    Parent ! {finish, self(), Len},
 	    receive_death(Parent, NextSocketData, 0);
 	{tcp_closed, NextSocketData} ->
-	    ?DEBUG("[~p, ~p] next data socket broken ~n", [?MODULE, ?LINE]); 
+	    ?DEBUG("[~p, ~p] next data socket broken ~n", [?MODULE, ?LINE]),
 	    file:close(Hdl),
 	    Parent ! {error, self(), "next socket data closed"},
 	    receive_death(Parent, NextSocketData, 0);
 	Any ->
 	    ?DEBUG("[~p, ~p] unknown: ~p~n", [?MODULE, ?LINE, Any]),
-	    loop_receive(Parent, SocketData, NextSocketData, Hdl, Len);
+	    loop_relay_receive(Parent, SocketData, NextSocketData, Hdl, Len)
     end.
 
 receive_death(Parent, NextSocketData, State) ->
@@ -235,6 +235,6 @@ receive_death(Parent, NextSocketData, State) ->
 	    ?DEBUG("[~p, ~p] close next socket data~n", [?MODULE, ?LINE]); 
 	Any ->
 	    ?DEBUG("[~p, ~p] unknown when wait for death: ~p~n", [?MODULE, ?LINE, Any]),
-	    receive_death(Parent, State)
+	    receive_death(Parent, NextSocketData, State)
     end.
 	    
